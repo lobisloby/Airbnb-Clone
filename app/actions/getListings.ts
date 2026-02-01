090f5d534d1d2d518dbf27a01591f6a71d8b3321
Ayoub Ouachr <ayoubiss.10@gmail.com>
2026-02-01 20:36:07 +0100

    fix(getListings): handle date overlaps and param issues, add Loader

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -3,6 +3,13 @@ import prisma from '@/app/libs/prismadb'
 
 export interface IListingParams{
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 
@@ -10,13 +17,72 @@ export default async function getListings(
     params: Promise<IListingParams>
 ){
     try{
-        const {userId} = await params;
+        const {
+            userId,
+            roomCount,
+            guestCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category 
+
+        } = await params;
 
         let query:any ={};
 
         if(userId){
             query.userId = userId;
         }
+
+        if(category){
+            query.category= category;
+        }
+
+        if(roomCount){
+            query.roomCount={
+                gte: +roomCount
+            }
+        }
+
+        if(guestCount){
+            query.guestCount={
+                gte: +guestCount
+            }
+        }
+        
+        if(bathroomCount){
+            query.bathroomCount={
+                gte: +bathroomCount
+            }
+        }
+
+        if(locationValue){
+            query.locationValue = locationValue;
+        }
+        
+
+        if(startDate && endDate){
+            query.NOT = {
+                reservations:{
+                    some:{
+                        OR: [
+                            {
+                                endDate:{gte: startDate},
+                                startDate: {lte: startDate}
+                            },
+                            {
+                                startDate:{lte: endDate},
+                                endDate:{gte: endDate}
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
+        
+
 
         const listings = await prisma.listing.findMany({
             where:query,
@@ -32,7 +98,7 @@ export default async function getListings(
         
         return safeListings;
     }catch(error:any){
-        throw new Error(error);
+        throw new Error(error); 
         
     }
 }
